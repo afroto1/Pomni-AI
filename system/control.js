2720f5083d4302cc7e066ed2b454873ae4b641cb
afroto1 <afrotoaldgalfo2@gmail.com>
2026-03-23 06:27:39 +0200

    Update control.js

--- system/control.js
+++ system/control.js
@@ -36,8 +36,8 @@ const group = async (ctx, event, eventType) => {
       body: "A simple WhatsApp bot for beginners, by VENOM",
       mentions: author ? [author, ...participants] : participants,
       newsletter: {
-      name: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
-      jid: '120363225356834044@newsletter'
+      name: '𝐓𝐄𝐀𝐌 𝐵𝛩𝑇 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 𝐁𝐋𝐀𝐂𝐊 🇪🇬 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
+      jid: '120363291329944922@newsletter'
     },
       big: ["remove", "add"].includes(eventType)
     });
