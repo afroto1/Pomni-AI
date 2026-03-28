0e4a373bb2bd71ba2410cb5f23a92fffc07a3ee5
afroto1 <afrotoaldgalfo2@gmail.com>
2026-03-29 00:47:48 +0200

    Update Mp3_1.js

--- plugins/Mp3_1.js
+++ plugins/Mp3_1.js
@@ -1,24 +1,13 @@
-let handler = async (m, { conn }) => {
+let handler = async (m, { conn, usedPrefix, __dirname, text, isPrems }) => {
 
-    const vn = 'https://files.catbox.moe/iaimvj.mp3';
-
-    conn.sendPresenceUpdate('recording', m.chat);
-
-    await conn.sendMessage(
-        m.chat,
-        {
-            audio: { url: vn },
-            ptt: true,
-            mimetype: 'audio/mpeg',
-            fileName: 'deja de llorar.mp3'
-        },
-        { quoted: m }
-    );
+    const vn = './media/بودعك.mp3';
+  conn.sendPresenceUpdate('recording', m.chat);
+  conn.sendMessage(m.chat, {audio: {url: vn}, ptt: true, mimetype: 'audio/mpeg', fileName: `deja de llorar.mp3`}, {quoted: m});
 };
 
-handler.help = ['notification'];
-handler.tags = ['notification'];
-handler.command = ['الوداع', 'بودعك'];
+handler.help = ['notification']
+handler.tags = ['notification']
+handler.command = ['الوداع ','بودعك'] 
 handler.customPrefix = /^(بودعك|الوداع)$/i;
-
-export default handler;
+handler.command = new RegExp;
+export default handler
