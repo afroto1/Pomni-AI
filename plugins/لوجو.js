c0e2cb7fee0e9985f029eaf0d43000d7650ebe03
afroto1 <afrotoaldgalfo2@gmail.com>
2026-03-29 19:27:16 +0200

    Update لوجو.js

--- plugins/لوجو.js
+++ plugins/لوجو.js
@@ -8,36 +8,36 @@ async function handler(m, { conn }) {
 
     try {
         const data = [
-            { name: "facebook", img: "https://cdn-icons-png.flaticon.com/512/124/124010.png" },
-            { name: "instagram", img: "https://cdn-icons-png.flaticon.com/512/2111/2111463.png" },
-            { name: "whatsapp", img: "https://cdn-icons-png.flaticon.com/512/733/733585.png" },
-            { name: "youtube", img: "https://cdn-icons-png.flaticon.com/512/1384/1384060.png" },
-            { name: "twitter", img: "https://cdn-icons-png.flaticon.com/512/733/733579.png" },
-            { name: "tiktok", img: "https://cdn-icons-png.flaticon.com/512/3046/3046121.png" },
-            { name: "snapchat", img: "https://cdn-icons-png.flaticon.com/512/733/733576.png" },
-            { name: "telegram", img: "https://cdn-icons-png.flaticon.com/512/2111/2111646.png" },
-            { name: "linkedin", img: "https://cdn-icons-png.flaticon.com/512/174/174857.png" },
-            { name: "pinterest", img: "https://cdn-icons-png.flaticon.com/512/733/733558.png" },
-            { name: "spotify", img: "https://cdn-icons-png.flaticon.com/512/174/174872.png" },
-            { name: "netflix", img: "https://cdn-icons-png.flaticon.com/512/5977/5977590.png" },
-            { name: "amazon", img: "https://cdn-icons-png.flaticon.com/512/5968/5968784.png" },
-            { name: "paypal", img: "https://cdn-icons-png.flaticon.com/512/174/174861.png" },
-            { name: "discord", img: "https://cdn-icons-png.flaticon.com/512/2111/2111370.png" },
-            { name: "reddit", img: "https://cdn-icons-png.flaticon.com/512/2111/2111589.png" },
-            { name: "skype", img: "https://cdn-icons-png.flaticon.com/512/174/174869.png" },
-            { name: "viber", img: "https://cdn-icons-png.flaticon.com/512/2111/2111708.png" },
-            { name: "slack", img: "https://cdn-icons-png.flaticon.com/512/2111/2111615.png" },
-            { name: "zoom", img: "https://cdn-icons-png.flaticon.com/512/5968/5968756.png" },
-            { name: "google", img: "https://cdn-icons-png.flaticon.com/512/300/300221.png" },
-            { name: "apple", img: "https://cdn-icons-png.flaticon.com/512/0/747.png" },
-            { name: "github", img: "https://cdn-icons-png.flaticon.com/512/733/733553.png" },
-            { name: "twitch", img: "https://cdn-icons-png.flaticon.com/512/2111/2111668.png" },
-            { name: "ebay", img: "https://cdn-icons-png.flaticon.com/512/732/732221.png" },
-            { name: "wechat", img: "https://cdn-icons-png.flaticon.com/512/2111/2111728.png" },
-            { name: "messenger", img: "https://cdn-icons-png.flaticon.com/512/733/733547.png" },
-            { name: "quora", img: "https://cdn-icons-png.flaticon.com/512/733/733600.png" },
-            { name: "tripadvisor", img: "https://cdn-icons-png.flaticon.com/512/733/733597.png" },
-            { name: "uber", img: "https://cdn-icons-png.flaticon.com/512/732/732200.png" }
+            { name: "facebook", img: "https://cdn.simpleicons.org/facebook/1877F2" },
+            { name: "instagram", img: "https://cdn.simpleicons.org/instagram/E4405F" },
+            { name: "whatsapp", img: "https://cdn.simpleicons.org/whatsapp/25D366" },
+            { name: "youtube", img: "https://cdn.simpleicons.org/youtube/FF0000" },
+            { name: "twitter", img: "https://cdn.simpleicons.org/twitter/1DA1F2" },
+            { name: "tiktok", img: "https://cdn.simpleicons.org/tiktok/000000" },
+            { name: "snapchat", img: "https://cdn.simpleicons.org/snapchat/FFFC00" },
+            { name: "telegram", img: "https://cdn.simpleicons.org/telegram/26A5E4" },
+            { name: "linkedin", img: "https://cdn.simpleicons.org/linkedin/0A66C2" },
+            { name: "spotify", img: "https://cdn.simpleicons.org/spotify/1DB954" },
+            { name: "netflix", img: "https://cdn.simpleicons.org/netflix/E50914" },
+            { name: "amazon", img: "https://cdn.simpleicons.org/amazon/FF9900" },
+            { name: "paypal", img: "https://cdn.simpleicons.org/paypal/00457C" },
+            { name: "discord", img: "https://cdn.simpleicons.org/discord/5865F2" },
+            { name: "reddit", img: "https://cdn.simpleicons.org/reddit/FF4500" },
+            { name: "skype", img: "https://cdn.simpleicons.org/skype/00AFF0" },
+            { name: "viber", img: "https://cdn.simpleicons.org/viber/7360F2" },
+            { name: "slack", img: "https://cdn.simpleicons.org/slack/4A154B" },
+            { name: "zoom", img: "https://cdn.simpleicons.org/zoom/2D8CFF" },
+            { name: "google", img: "https://cdn.simpleicons.org/google/4285F4" },
+            { name: "apple", img: "https://cdn.simpleicons.org/apple/000000" },
+            { name: "github", img: "https://cdn.simpleicons.org/github/181717" },
+            { name: "twitch", img: "https://cdn.simpleicons.org/twitch/9146FF" },
+            { name: "ebay", img: "https://cdn.simpleicons.org/ebay/E53238" },
+            { name: "wechat", img: "https://cdn.simpleicons.org/wechat/07C160" },
+            { name: "messenger", img: "https://cdn.simpleicons.org/messenger/00B2FF" },
+            { name: "quora", img: "https://cdn.simpleicons.org/quora/B92B27" },
+            { name: "tripadvisor", img: "https://cdn.simpleicons.org/tripadvisor/34E0A1" },
+            { name: "uber", img: "https://cdn.simpleicons.org/uber/000000" },
+            { name: "airbnb", img: "https://cdn.simpleicons.org/airbnb/FF5A5F" }
         ];
 
         const item = data[Math.floor(Math.random() * data.length)];
@@ -82,7 +82,6 @@ handler.before = async (m) => {
 
     const answer = m.text.toLowerCase().trim();
 
-    // 🔥 ذكاء في الإجابة (مش لازم تطابق 100%)
     if (answer.includes(game.answer)) {
         clearTimeout(game.timeout);
         delete global.gameActive[m.chat];
