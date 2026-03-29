fb0a83062da40d70d25aeff46450b3ca4b8f6a08
afroto1 <afrotoaldgalfo2@gmail.com>
2026-03-30 00:25:27 +0200

    Update بوت.js

--- plugins/بوت.js
+++ plugins/بوت.js
@@ -1,13 +1,17 @@
-let handler = async (m) => {
-  m.reply('╮───────────────────────╭ـ
-مرحبا بك فى بوت بلاك 🤖
+let handler = async (m, { conn, text }) => {
 
-مثال:
-│❏ بوت من نيكولا تسلا
-│❏ بوت افضل انمى
-│❏ بوت هات فزورة أو لغز
-╰───────────────────────╯ ')
+  if (!text) throw '*أدخــل الـسـؤال !*'
+
+  let msg = `*تــم اســتـلام ســؤالـك :* ${text}`
+
+  await conn.sendMessage(m.chat, {
+    text: msg,
+    mentions: [m.sender]
+  }, { quoted: m })
 }
 
 handler.command = /^بوت$/i
+handler.group = true
+handler.tags = ['test']
+
 export default handler
